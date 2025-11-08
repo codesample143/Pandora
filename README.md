672dc76cb8363cc054df0b1103ed64dbe275f6ae
= <definitelynotkevincs@gmail.com>
2025-11-07 22:49:46 -0500

    modifying basic function stubs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Pandora
+
+This will be a CNN based custom Neural-Network eventually implemented entirely in scratch. 
+
---
+++ functions.py
@@ -0,0 +1,8 @@
+
+import numpy as np
+
+def matrix_multiplication(A, B):
+    if len(A) != len(B):
+        raise TypeError(f"Size of {A} != Size of {B}")
+    
+    pass
