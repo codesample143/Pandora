8ba0e44db37386b35c0d8fc6440186daaccf273c
= <definitelynotkevincs@gmail.com>
2025-11-10 01:14:53 -0500

    added architecture

--- board.py
+++ board.py
@@ -1,5 +1,42 @@
 import chess
-
-
+import numpy as np
+import eval
+import torch
+# 8x8 grid of pieces.
+# OHE? options for piece encoding: OHE, Keys, Pairs, 
+# MLP that takes board as input (feed forward neural network)
 board = chess.Board()
-print(board)
+
+# use KV for piece encoding, feed into MLP
+# 8x8 matrix of values that correspond to pieces
+
+# P -> 1, B-> 2, N->3, R->4, Q->5, K->6, p -> 7, b->8, n -> 9, r->10 q->11, k->12
+# lowcase black
+# function to encoding a board
+# output: [[]]
+# A1 -> starts with White Rook ends with A8
+def enc(board):
+    # return matrix
+    ret = [] 
+    current_row = []
+    for square in chess.SQUARE_NAMES:
+        try:
+            current_row.append((p:= board.piece_at(chess.parse_square(square))).piece_type + int(not p.color) * 6)
+            if(len(current_row) == 8):
+                ret.append(current_row)
+                current_row = []
+        except:
+            current_row.append(0)
+            if(len(current_row) == 8):
+                ret.append(current_row)
+                current_row = []
+            continue
+    return torch.tensor(ret).to(dtype=torch.float32)
+
+# 8x8 2d tensor->flatten 1d vector -> 
+enc_board = enc(board).flatten()
+
+# everything happens from this board state, value, minmax
+# 64 x 1 -> 128- > 32 nodes -> 10 nodes -> 1
+model = eval.Evaluation([(64, 'relu'), (128, 'relu'), (32, 'relu'), (10, 'relu'), (1, 'sigmoid')])
+print(model(enc_board))
---
+++ eval.py
@@ -0,0 +1,34 @@
+from torch import nn
+import torch.nn.functional as F
+import torch
+class Evaluation(nn.Module):
+    # [(layersize, evalf) ...]
+    def __init__(self, layers): # Prev implementation: input_size, hidden_size1, hidden_size2, hidden_size3, output_size,
+        super(Evaluation, self).__init__()
+        self.layers = layers
+        self.fcs = []
+        for layer_pair_index in range(1, len(layers)):
+            last_size, _ = layers[layer_pair_index - 1]
+            current_size, _ = layers[layer_pair_index]
+            self.fcs.append(nn.Linear(last_size, current_size))
+        # self.fc1 = nn.Linear(input_size, hidden_size1)
+        # self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        # self.fc3 = nn.Linear(hidden_size2, hidden_size3)
+        # self.fc4 = nn.Linear(hidden_size3, output_size)
+    def forward(self, x):
+        def apply(s):
+            if s == "relu":
+                return F.relu
+            elif s == 'sigmoid':
+                return F.sigmoid
+        _, initf = self.layers[0]
+        x = apply(initf)(self.fcs[0](x))
+
+        for i in range(1, len(self.fcs)):
+            _, cf = self.layers[i]
+            x = apply(cf)(self.fcs[i](x))
+        # x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc3(x))
+        # x = F.sigmoid(self.fc4(x)) #make sure we get value between 0 and 1
+        return x
